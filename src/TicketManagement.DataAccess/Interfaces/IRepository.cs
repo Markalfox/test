a5af842efad94865f2858abd39c6fd060f94092c
Markalfox <55929539+Markalfox@users.noreply.github.com>
2020-09-09 09:25:40 +0300

    Added a folder with interface IRepository

---
+++ src/TicketManagement.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TicketManagement.DataAccess.Interfaces
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        IEnumerable<T> GetAll(); // get all objects
+
+        T GetById(int id); // get one object
+
+        void Create(T item); // add object
+
+        void Update(T item); // change object
+
+        void Delete(int id); // delete object
+    }
+}
