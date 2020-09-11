fdd438506610ba076fe6e23f5e96e2acf2bf3eb5
Markalfox <55929539+Markalfox@users.noreply.github.com>
2020-09-11 11:29:55 +0300

    Create ICrud interface

---
+++ src/TicketManagement.BusinessLogic/Interfaces/ICrud.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Ticketmanagement.BusinessLogic.Interfaces
+{
+    public interface ICrud<T>
+        where T : class
+    {
+        IEnumerable<T> GetAllElements(); // get all objects
+
+        T GetElementById(int id); // get one object
+
+        void CreateElement(T item); // add object
+
+        void UpdateElement(T item); // change object
+
+        void DeleteElement(int id); // delete object
+    }
+}
