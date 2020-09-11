9c7bdb8312fc200135a15be82a7b99ccf95b1894
Markalfox <55929539+Markalfox@users.noreply.github.com>
2020-09-11 22:51:13 +0300

    Implement ICrud for AreaService and add validation class by name AreaServiceValidation for AreaService

---
+++ src/TicketManagement.BusinessLogic/Services/AreaService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Ticketmanagement.BusinessLogic.Data_Transfer_Objects;
+using Ticketmanagement.BusinessLogic.Interfaces;
+using Ticketmanagement.BusinessLogic.Validations;
+using TicketManagement.DataAccess.Entities;
+using TicketManagement.DataAccess.Repositories.SqlRepository;
+
+namespace Ticketmanagement.BusinessLogic.Services
+{
+    public class AreaService : ICrud<AreaDto>
+    {
+        // FIELDS
+        private readonly SqlAreaRepository _areaRepository;
+
+        // CONSTRUCTORS
+        public AreaService(SqlAreaRepository repository)
+            => _areaRepository = repository;
+
+        // METHODS
+        public void CreateElement(AreaDto item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            AreaServiceValidation.CheckCoordX(item);
+            AreaServiceValidation.CheckCoordY(item);
+            AreaServiceValidation.CheckDescription(GetAllElements().Where(x => x.LayoutId == item.LayoutId), item);
+
+            _areaRepository.Create(Mapping().Map<AreaDto, AreaEntity>(item));
+        }
+
+        public void DeleteElement(int id)
+        {
+            if (AreaServiceValidation.CheckId(GetAllElements().Count(), id))
+            {
+                _areaRepository.Delete(id);
+            }
+        }
+
+        public IEnumerable<AreaDto> GetAllElements()
+        {
+            List<AreaDto> elements = new List<AreaDto>();
+
+            foreach (var i in Mapping().Map<List<AreaDto>>(_areaRepository.GetAll()))
+            {
+                elements.Add(i);
+            }
+
+            return elements;
+        }
+
+        public AreaDto GetElementById(int id)
+        {
+            if (AreaServiceValidation.CheckId(GetAllElements().Count(), id))
+            {
+                return Mapping().Map<AreaDto>(_areaRepository.GetById(id));
+            }
+
+            return null;
+        }
+
+        public void UpdateElement(AreaDto item)
+        {
+            if (item != null)
+            {
+                _areaRepository.Update(Mapping().Map<AreaDto, AreaEntity>(item));
+            }
+        }
+
+        private static Mapper Mapping()
+        {
+            Mapper mapper = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<AreaEntity, AreaDto>()));
+
+            return mapper;
+        }
+    }
+}
---
+++ src/TicketManagement.BusinessLogic/Validations/AreaServiceValidation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ticketmanagement.BusinessLogic.Data_Transfer_Objects;
+
+namespace Ticketmanagement.BusinessLogic.Validations
+{
+    public static class AreaServiceValidation
+    {
+        public static void CheckCoordX(AreaDto item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            else
+            {
+                if (item.CoordX < 0)
+                {
+                    Exception exception = new Exception("The X coordinate cannot be less than 0");
+                    throw exception;
+                }
+            }
+        }
+
+        public static void CheckCoordY(AreaDto item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            else
+            {
+                if (item.CoordY < 0)
+                {
+                    Exception exception = new Exception("The Y coordinate cannot be less than 0");
+                    throw exception;
+                }
+            }
+        }
+
+        public static void CheckDescription(IEnumerable<AreaDto> elements, AreaDto item)
+        {
+            if (elements.Any(x => x.Description == item.Description))
+            {
+                Exception exception = new Exception("The same description");
+                throw exception;
+            }
+        }
+
+        public static bool CheckId(int length, int id)
+        {
+            if ((id > 0) && (id < length))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
