f4a5b82218c40e962ea33056c018c6be08e0c4ec
Markalfox <55929539+Markalfox@users.noreply.github.com>
2020-09-11 11:34:15 +0300

    Add class by name EventServise and implement ICrud for this class. Add class by name EventServiceValidation

---
+++ src/TicketManagement.BusinessLogic/Services/EventService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Ticketmanagement.BusinessLogic.Data_Transfer_Objects;
+using Ticketmanagement.BusinessLogic.Interfaces;
+using Ticketmanagement.BusinessLogic.Validations;
+using TicketManagement.DataAccess.Entities;
+using TicketManagement.DataAccess.Repositories.SqlRepository;
+
+namespace Ticketmanagement.BusinessLogic.Services
+{
+    public class EventService : ICrud<EventDto>
+    {
+        // FIELDS
+        private readonly SqlEventRepository _eventRepository;
+
+        // CONSTRUCTORS
+        public EventService(SqlEventRepository repository)
+            => _eventRepository = repository;
+
+        // METHODS
+        public void CreateElement(EventDto item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            else
+            {
+                if (item.StartDate > item.EndDate)
+                {
+                    var exception = new Exception("The event start date cannot be greater than the event end date");
+                    throw exception;
+                }
+            }
+
+            EventServiceValidation.CheckName(GetAllElements(), item);
+            EventServiceValidation.CheckDate(GetAllElements().Where(x => x.LayoutId == item.LayoutId), item);
+
+            _eventRepository.Create(Mapping().Map<EventDto, EventEntity>(item));
+        }
+
+        public void DeleteElement(int id)
+        {
+            if (EventServiceValidation.CheckId(GetAllElements().Count(), id))
+            {
+                _eventRepository.Delete(id);
+            }
+        }
+
+        public IEnumerable<EventDto> GetAllElements()
+        {
+            List<EventDto> elements = new List<EventDto>();
+
+            foreach (var i in Mapping().Map<List<EventDto>>(_eventRepository.GetAll()))
+            {
+                elements.Add(i);
+            }
+
+            return elements;
+        }
+
+        public EventDto GetElementById(int id)
+        {
+            if (EventServiceValidation.CheckId(GetAllElements().Count(), id))
+            {
+                return Mapping().Map<EventDto>(_eventRepository.GetById(id));
+            }
+
+            return null;
+        }
+
+        public void UpdateElement(EventDto item)
+        {
+            if (!EventServiceValidation.CheckNull(item))
+            {
+                _eventRepository.Update(Mapping().Map<EventDto, EventEntity>(item));
+            }
+        }
+
+        private static Mapper Mapping()
+        {
+            Mapper mapper = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<EventEntity, EventDto>()));
+
+            return mapper;
+        }
+    }
+}
---
+++ src/TicketManagement.BusinessLogic/Validations/EventServiceValidation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ticketmanagement.BusinessLogic.Data_Transfer_Objects;
+
+namespace Ticketmanagement.BusinessLogic.Validations
+{
+    public static class EventServiceValidation
+    {
+        public static void CheckName(IEnumerable<EventDto> events, EventDto item)
+        {
+            if (events.Any(x => x.Name == item.Name))
+            {
+                Exception exception = new Exception("An event with this name already exists");
+                throw exception;
+            }
+        }
+
+        public static void CheckDate(IEnumerable<EventDto> events, EventDto item)
+        {
+            if (events.Any(x => x.StartDate >= item.StartDate && x.StartDate <= item.EndDate))
+            {
+                Exception exception = new Exception("Change the date or location of the event. Another event is already planned for this period at this place");
+                throw exception;
+            }
+
+            if (events.Any(x => x.EndDate >= item.StartDate && x.EndDate <= item.EndDate))
+            {
+                Exception exception = new Exception("Change the end date or venue of the event. At this time, another event will take place in this place");
+                throw exception;
+            }
+
+            if (events.Any(x => (x.EndDate >= item.EndDate && x.StartDate <= item.StartDate) || (x.EndDate <= item.EndDate && x.StartDate >= item.StartDate)))
+            {
+                Exception exception = new Exception("At this time, another event will take place in this place");
+                throw exception;
+            }
+        }
+
+        public static bool CheckId(int length, int id)
+        {
+            if ((id > 0) && (id < length))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool CheckNull(EventDto item)
+        {
+            return item == null;
+        }
+    }
+}
