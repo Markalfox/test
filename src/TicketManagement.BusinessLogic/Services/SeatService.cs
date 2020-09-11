71efb9bd1755ba9305b45b81b87d3ff0bbad9441
Markalfox <55929539+Markalfox@users.noreply.github.com>
2020-09-11 21:26:46 +0300

    Implement ICrud for SeatService and add validation class for SeatService

---
+++ src/TicketManagement.BusinessLogic/Services/SeatService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Ticketmanagement.BusinessLogic.Data_Transfer_Objects;
+using Ticketmanagement.BusinessLogic.Interfaces;
+using Ticketmanagement.BusinessLogic.Validations;
+using TicketManagement.DataAccess.Entities;
+using TicketManagement.DataAccess.Repositories.SqlRepository;
+
+namespace Ticketmanagement.BusinessLogic.Services
+{
+    public class SeatService : ICrud<SeatDto>
+    {
+        // FIELDS
+        private readonly SqlSeatRepository _seatRepository;
+
+        // CONSTRUCTORS
+        public SeatService(SqlSeatRepository repository)
+            => _seatRepository = repository;
+
+        // METHODS
+        public void CreateElement(SeatDto item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            else
+            {
+                if (item.Number < 0 || item.Row < 0)
+                {
+                    var exception = new Exception("The number or row cannot be negative");
+                    throw exception;
+                }
+            }
+
+            SeatServiceValidation.CheckNumber(GetAllElements().Where(x => x.AreaId == item.AreaId), item);
+            SeatServiceValidation.CheckRow(GetAllElements().Where(x => x.AreaId == item.AreaId), item);
+
+            _seatRepository.Create(Mapping().Map<SeatDto, SeatEntity>(item));
+        }
+
+        public void DeleteElement(int id)
+        {
+            if (SeatServiceValidation.CheckId(GetAllElements().Count(), id))
+            {
+                _seatRepository.Delete(id);
+            }
+        }
+
+        public IEnumerable<SeatDto> GetAllElements()
+        {
+            List<SeatDto> elements = new List<SeatDto>();
+
+            foreach (var i in Mapping().Map<List<SeatDto>>(_seatRepository.GetAll()))
+            {
+                elements.Add(i);
+            }
+
+            return elements;
+        }
+
+        public SeatDto GetElementById(int id)
+        {
+            if (SeatServiceValidation.CheckId(GetAllElements().Count(), id))
+            {
+                return Mapping().Map<SeatDto>(_seatRepository.GetById(id));
+            }
+
+            return null;
+        }
+
+        public void UpdateElement(SeatDto item)
+        {
+            if (item != null)
+            {
+                _seatRepository.Update(Mapping().Map<SeatDto, SeatEntity>(item));
+            }
+        }
+
+        private static Mapper Mapping()
+        {
+            Mapper mapper = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<SeatEntity, SeatDto>()));
+
+            return mapper;
+        }
+    }
+}
---
+++ src/TicketManagement.BusinessLogic/Validations/SeatServiceValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ticketmanagement.BusinessLogic.Data_Transfer_Objects;
+
+namespace Ticketmanagement.BusinessLogic.Validations
+{
+    public static class SeatServiceValidation
+    {
+        public static void CheckRow(IEnumerable<SeatDto> seats, SeatDto item)
+        {
+            if (seats.Any(x => x.Row == item.Row))
+            {
+                Exception exception = new Exception("This row already exists in the current zone");
+                throw exception;
+            }
+        }
+
+        public static void CheckNumber(IEnumerable<SeatDto> seats, SeatDto item)
+        {
+            if (seats.Any(x => x.Number == item.Number))
+            {
+                Exception exception = new Exception("This number is already in the current row");
+                throw exception;
+            }
+        }
+
+        public static bool CheckId(int length, int id)
+        {
+            if ((id > 0) && (id < length))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
